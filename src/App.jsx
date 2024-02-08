eb7f12570b7ee825a55ac1dfe74488ac3f00077b
mkg1 <gehring.mg@gmail.com>
2024-02-07 21:24:23 -0500

    Display map from main app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import Nav from './components/Nav'
 import Map from './components/Map'
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = import.meta.env.VITE_MAP_BOX_KEY;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(import.meta.env.MAP_BOX_KEY, "keyyyy")
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+    
   return (
-    <>
+    <div className="App">
       <Nav />
-      <Map />
-    </>
-  )
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 }
 
-export default App
+export default App;
