8ff25f85648e096ff5fac942e10bbe45c59725e7
mkg1 <gehring.mg@gmail.com>
2024-02-09 12:01:14 -0500

    WIP - stoping point with markers showing up (wrong loc)

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
 import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import Nav from './components/Nav'
-import Map from './components/Map'
+// import Map from './components/Map'
 import mapboxgl from 'mapbox-gl';
+import { DEFAULT_MARKERS } from './constants'
+
 
 mapboxgl.accessToken = import.meta.env.VITE_MAP_BOX_KEY;
 
 
 function App() {
-  console.log(import.meta.env.MAP_BOX_KEY, "keyyyy")
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-78.8986);
   const [lat, setLat] = useState(35.9940);
   const [zoom, setZoom] = useState(9);
 
+
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    // initialize map only once
+    if (map.current) return;
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v12",
       center: [lng, lat],
       zoom: zoom,
-    });
-  });
+    }); 
+  }, []);
+  
+  useEffect(() => {
+    console.log("wheeeee")
+    if(map.current) {
+      for (const feature of DEFAULT_MARKERS.features) {
+      // make a marker for each feature and add to the map
+      const el = document.getElementsByClassName('marker');
+      el.className = 'marker'
+      new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map.current)
+      }}
+  }, [])
+
+
 
   if (map.current) {
+    console.log("but do we make it here?")
     map.current.on('move', () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
@@ -33,14 +50,12 @@ function App() {
       });  
   }
     
-    
   return (
     <div className="App">
       <Nav />
       <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-
       <div ref={mapContainer} className="map-container" />
     </div>
   );
---
+++ src/constants.js
@@ -0,0 +1,77 @@
+export const DEFAULT_MARKERS = {
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -78.921881,
+            35.915008
+          ],
+          "type": "Point"
+        },
+        "id": "14dcda99e5d3e0ce99bada89f79f8749"
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -78.899329,
+            36.03193
+          ],
+          "type": "Point"
+        },
+        "id": "154d72867de0ac11e21a10b2c2af2988"
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -78.931683,
+            35.986073
+          ],
+          "type": "Point"
+        },
+        "id": "374674908c66090bf53c11bf876b7301"
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -78.935113,
+            35.985207
+          ],
+          "type": "Point"
+        },
+        "id": "54e56862f1afb7bfcdea39aa9bbe339b"
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -78.910763,
+            35.981684
+          ],
+          "type": "Point"
+        },
+        "id": "6c75952e6ca8291b749a4064f284dea0"
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -78.935888,
+            35.98244
+          ],
+          "type": "Point"
+        },
+        "id": "ee4e0e7a387036ede80ebb2dc305fcb4"
+      }
+    ],
+    "type": "FeatureCollection"
+  }
